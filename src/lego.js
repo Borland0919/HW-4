be3b93617bb33a5ed077c80b63adc4ce748777dc
AlexKamov <lapkins.head@gmail.com>
2022-12-15 18:21:00 +0200

    HW-#9-Upd-Lego_Cataloge-Hidden+

--- src/lego.js
+++ src/lego.js
@@ -1,10 +1,22 @@
 const detailsImage = document.querySelector(".details-image-head");
 const detailsTitle = document.querySelector(".details-title-head");
 const anchorElements = document.querySelectorAll(".class-anchor");
+const mainElement = document.querySelector("main");
+const hiddenButonX = document.querySelector("#button-hidden-x");
+const HIDDEN = "hidden";
+
+function showDetails(){
+    mainElement.classList.remove(HIDDEN);
+}
+
+function addDetails(){
+    mainElement.classList.add(HIDDEN);
+}
 
 function setDetails(anchor){
     const dataImage = anchor.getAttribute("data-details-image");
     detailsImage.src = dataImage;
+    showDetails();
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
 }
 
@@ -13,3 +25,5 @@ for(let i = 0; i < anchorElements.length; i++){
         setDetails(anchorElements[i]);
     })
 }
+
+hiddenButonX.addEventListener("click",addDetails);
