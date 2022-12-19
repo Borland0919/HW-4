270f62cffbd313d390daa6a5c221631a77c2a17c
AlexKamov <lapkins.head@gmail.com>
2022-12-19 13:53:28 +0200

    HW-#10-Final_Upd_LegoCataloge+

--- src/lego.js
+++ src/lego.js
@@ -1,12 +1,19 @@
 const detailsImage = document.querySelector(".details-image-head");
 const detailsTitle = document.querySelector(".details-title-head");
+const music = document.querySelector(".music");
 const anchorElements = document.querySelectorAll(".class-anchor");
 const mainElement = document.querySelector("main");
 const hiddenButonX = document.querySelector("#button-hidden-x");
+const classSectionHead = document.querySelector(".class-section-head");
 const HIDDEN = "hidden";
+const isPoint = "is-point";
 
 function showDetails(){
     mainElement.classList.remove(HIDDEN);
+    classSectionHead.classList.add(isPoint);
+    setTimeout(function(){
+        classSectionHead.classList.remove(isPoint);
+    })
 }
 
 function addDetails(){
@@ -16,8 +23,14 @@ function addDetails(){
 function setDetails(anchor){
     const dataImage = anchor.getAttribute("data-details-image");
     detailsImage.src = dataImage;
-    showDetails();
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
+    music.src = anchor.getAttribute("data-music");
+    music.play();
+    setTimeout(function(){
+       music.pause();
+    },100000)
+    showDetails();
+    
 }
 
 for(let i = 0; i < anchorElements.length; i++){
