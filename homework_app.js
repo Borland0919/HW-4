28c62e5ded611d3f67125be2b377bb63065ddef7
AlexKamov <lapkins.head@gmail.com>
2023-01-05 00:47:51 +0200

    HW-#14

--- homework_app.js
+++ homework_app.js
@@ -1,49 +1,76 @@
-function getRandomNumber(min,max){
-  min = Math.trunc(min);
-  max = Math.trunc(max);
+//Task - 1
+let str1 = "apple";
+let str2 = "puppy";
+function colorString(str1,str2){
+  const arStr1 = Array.from(str1);
+  const arStr2 = Array.from(str2);
+  let arResult = [];
+  
+  if(arStr1.length === arStr2.length){
+    arResult = arStr2.map(function(number,index){
+      const arMoment = arStr1.filter(function(number){
+        if(number == arStr2[index])
+        return number;
+      })
+        let colorElement = "";
+        if(number === arStr1[index]){
+          colorElement = "green";
+        }
+        else if(arMoment[0] == arStr2[index]){
+          colorElement = "yellow";
+        }
+        else
+           colorElement = "red";
 
-  return min + Math.trunc(Math.random() * (max * min + 1));
-}
-const arHtml = [0, 1, 0, 1, 1, 0,];
-
-function getHtmlUl(ar){
-  const html = [];
-  html.push('<ul class="list-class">');
-  let strClass = "";
-  for(let i = 0; i < ar.length ; i++){
-    html.push('<li class="item class">');
-    if(ar[i] == 0){
-       strClass = "white";
-      }
-    else {
-       strClass = "black";
-      }
-   html.push(`<div class="${strClass}"></div></li>`);
+    return colorElement; 
+  }) 
   }
-  html.push('</ul>');
-  let res = html.join("");
-  return res;
+  else
+  console.log(-1);
+
+  return arResult;
 }
+console.log(colorString("pappy","apple"));
 
-let resHtml = getHtmlUl(arHtml);
-console.log(resHtml);
+//Task - 1 (Option B)
 
-const matrix = [[1,2][3,4][5,6]];
-
-function transpMax(matrix){
-  const matrixNew = [];
-  let rows = matrix[0].length;
-  let columns = matrix.length;
-
-  for(let j = 0; j < rows; j++)
-  {
-    matrixNew.push([])
-    for(let i = 0;i < columns;i++){
-      matrixNew[j].push(matrix[i][j]);
-    }
+let str3 = "apple";
+let str4 = "puppy";
+function colorString1(str3,str4){
+  const arStr3 = Array.from(str3);
+  const arStr4 = Array.from(str4);
+  let arResult = [];
+  
+  if(arStr3.length === arStr4.length){
+    arResult = arStr4.map(function(number,index){
+      if(arStr3.indexOf(number) === -1){
+        return "red";
+      }
+      else if (arStr3.indexOf(number) != index && arStr3.indexOf(number) > 0){
+        return "yellow";
+      }
+      if(number === arStr3[index]){
+        return "green";
+      }
+    })
+    return arResult;
   }
-
-  return matrixNew;
 }
 
-console.log(transpMax(matrix));
+console.log(colorString1("pippy","apple"));
+
+//Task - 2
+function getNumbersWithDigitsAmount(digitsAmount,array){
+   let numbersOfDigits = digitsAmount;
+   const res = array.filter(function (number){
+       if (number < 0) {
+         number = -number;
+        }
+       number = String(number);
+       if(numbersOfDigits === number.length){
+        return Number(number);
+       }
+    })
+    return res;
+}
+console.log(getNumbersWithDigitsAmount(2,[123,34,1,5678,12,98,95,-54,-142]));
