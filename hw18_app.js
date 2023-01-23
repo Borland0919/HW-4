b2859b29d30b1c31618ffc460d8f75f4971962de
AlexKamov <lapkins.head@gmail.com>
2023-01-23 21:31:09 +0200

    HW-#19

--- hw18_app.js
+++ hw18_app.js
@@ -1,70 +1,76 @@
-function createEmpolyee(id,name,birthYear,salary,city,country){
-    return {id, name, birthYear, salary, adress: {city, country}};
-  
+function createEmployee(id, name, birthYear, salary, city, country) {
+    return { id, name, birthYear, salary, address: { city, country }}
 }
 
 const employees = [
-    createEmpolyee(123, "Tolya", 2000, 15000, "Lod", "Israel"),
-    createEmpolyee(124, "David", 1975, 15500, "Tel Aviv", "Israel"),
-    createEmpolyee(125, "Sara", 1985, 20000, "New York", "USA"),
-    createEmpolyee(126, "Abraham", 1990, 13000, "London", "UK"),
-    createEmpolyee(127, "Moshe", 2000, 15000, "Rehovot", "Israel"),
-    createEmpolyee(128, "Goga", 1993, 10000, "Tbilisi", "Georgia"),
-    createEmpolyee(129, "Sasha", 2000, 25000, "London", "Israel"),
-    createEmpolyee(130, "Victor", 2003, 10000, "Arad", "Armenia")
+    createEmployee(123, "Tolya", 2000, 15000, "Lod", "Israel"),
+    createEmployee(124, "David", 1975, 15500, "Tel Aviv", "Israel"),
+    createEmployee(125, "Sara", 1985, 20000, "New York", "USA"),
+    createEmployee(126, "Abraham", 1990, 13000, "London", "UK"),
+    createEmployee(127, "Moshe", 2000, 15000, "Rehovot", "Israel"),
+    createEmployee(128, "Goga", 1993, 10000, "Tbilisi", "Georgia"),
+    createEmployee(129, "Sasha", 2000, 25000, "London", "Israel"),
+    createEmployee(130, "Victor", 2003, 10000, "Arad", "Armenia")
 ];
 
-//1
-function getEmployee(employees, id){
-    const worker = employees.find(function(employee){
-        return employee.id == id;
-    })
-    return worker;
+function getMostPopulated(employees){
+    const stringNumbersOfCountries = getStringOccurrensecOfCountries(employees);
+    const arraysCountries = Object.entries(stringNumbersOfCountries);
+    const populatedCountry = getMaxNumbers(arraysCountries);
+    console.log(`Most Populated Country is ${populatedCountry[0]} - ${populatedCountry[1]} employees from this country`);
 }
-console.log(getEmployee(employees, 127));
 
-//2
-function getEmployeesBySalary(employees, salaryFrom, salaryTo){
-    const sameSalary = employees.filter(function(workers){
-        if(workers.salary >= salaryFrom && workers.salary <= salaryTo){
-            return workers;
-        }
+function getStringOccurrensecOfCountries(employees){
+    const res = {};
+    employees.forEach(element => {
+     if(!res[element.address.country]){
+         res[element.address.country] = 1;
+     }
+      else {
+         res[element.address.country]++;
+      }
     });
-    return sameSalary;
-}
-console.log(getEmployeesBySalary(employees,5000,13000));
+    return res;
+ }
 
-//3
-function getEmployeesByCity(employees, city){
-     const fromCity =  employees.find(function(workers){
-        return workers.adress.city === city;
-     });
-     return fromCity;
+function getMaxNumbers(arraysCountries){
+    return arraysCountries.reduce(function (max,cur){
+        return cur[1] > max ? cur[1] : max;
+        });
 }
-console.log(getEmployeesByCity(employees,"London"));
 
-//4
-function getEmployeeNames(employees){
-    const allNames = employees.map(function (workers,index){
-      return index + 1 + "-" + workers.name;
-    });
+getMostPopulated(employees);
 
-    return allNames;    
+function getMostPopulatedCountries(employees, number){
+    const stringNumbersOfCountries = getStringOccurrensecOfCountries(employees);
+    const arraysCountries = Object.entries(stringNumbersOfCountries);
+    arraysCountries.sort(sorting);
+    number <= arraysCountries.length ? print(arraysCountries,number) : console.log("Error,incorect number of countrys.");
 }
-console.log(getEmployeeNames(employees));
 
-//5
-function sortEmployeesByAge(employees){
-  return employees.sort((worker1,worker2) => worker2.birthYear - worker1.birthYear);
+function getStringOccurrensec(employees){
+   const res = {};
+   employees.forEach(element => {
+    if(!res[element.address.country]){
+        res[element.address.country] = 1;
+    }
+     else {
+        res[element.address.country]++;
+     }
+   });
+   return res;
 }
-console.log(sortEmployeesByAge(employees));
 
-//6
-function budgetForSalary(employees){
-    const budget = employees.reduce(function(total,workers){
-       total += workers.salary;
-       return total
-    },0);
-    return budget;
+function sorting(elm1, elm2){
+    let res = elm2[1]- elm1[1];
+    return res;
+};
+
+function print(array, number){
+    for(let i = 0; i < number; i++){
+        {console.log(`${array[i][0]} -> ${array[i][1]}`);
+    }
+  }
 }
-console.log(`Budget for worker's salary is ${budgetForSalary(employees)} NIS`);
+
+getMostPopulatedCountries(employees, 1)
